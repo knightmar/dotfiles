f7534012bcbdf6de76903cdf09bfb540218f2cc2
arthur.petit-demouliere <arthur.petit-demouliere@epita.fr>
2026-09-12 15:18:56 +0200

    added things

--- waybar/scripts/powermenu.sh
+++ waybar/scripts/powermenu.sh
@@ -1,8 +1,9 @@
 # Options du menu
-choice=$(echo -e " Eteindre\n Redemarrer\n Deconnexion" | wofi --dmenu --width 200 --height 150)
+choice=$(echo -e " Eteindre\n Redemarrer\n Deconnexion\n Hyprlock" | wofi --dmenu --width 200 --height 150)
 
 case "$choice" in
     " Eteindre") systemctl poweroff ;;
     " Redemarrer") systemctl reboot ;;
     " Deconnexion") pkill Hyprland ;;
+    " Hyprlock") hyprlock;;
 esac
